cd6c84e07612cdb5f87425708d4998b8c9efdeb0
abdikeem <abdihakimabdirahman21@gmail.com>
2021-11-04 13:14:58 +0300

    app/views.py added

--- OnlineSchoolmgmt/app/views.py
+++ OnlineSchoolmgmt/app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import login,logout,authenticate
+from django.http import HttpResponse
+from . forms import *
 
 # Create your views here.
+
+def home(request):
+    notice = Notice.objects.all()
+    attendance = Attendance.objects.all()
+    marks = Marks.objects.all()
+
+    context = {
+        'notice':notice,
+        'attendance':attendance,
+        'marks':marks,
+    }
+
+    return render(request, 'app/home.html', context)
+
+def addAttendance(request):
